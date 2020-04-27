4e9b375d89593d99819a0e22754574de9efa7daf
dan.cao <caodan@linuxtoy.cn>
2020-04-27 14:30:35 +0800

    ENH: Add test image data
    Signed-off-by: dan.cao <caodan@linuxtoy.cn>

--- test.sh
+++ test.sh
@@ -6,17 +6,10 @@
 # Author: Dan.Cao <caodan@linuxtoy.cn>
 #--------------------------------------------------------------------
 
-INPUT_FILE="../imx378_raw10_1920x1080.raw"
+INPUT_FILE="test_data/imx378_RGGB_raw10_1920x1080.raw"
 WIDTH=1920
 HEIGHT=1080
 FORMAT=RAW10_UNPACKED
+CFA=RGGB
 
-#INPUT_FILE="../1DSM3_14bit_6144x3160.raw"
-#WIDTH=6144
-#HEIGHT=3160
-#FORMAT=RAW14_UNPACKED
-
-./test_demosaic -i ${INPUT_FILE} -o RGGB_${WIDTH}x${HEIGHT}.rgb -w ${WIDTH} -v ${HEIGHT} --format=${FORMAT} --cfa=RGGB
-./test_demosaic -i ${INPUT_FILE} -o GRBG_${WIDTH}x${HEIGHT}.rgb -w ${WIDTH} -v ${HEIGHT} --format=${FORMAT} --cfa=GRBG
-./test_demosaic -i ${INPUT_FILE} -o GBRG_${WIDTH}x${HEIGHT}.rgb -w ${WIDTH} -v ${HEIGHT} --format=${FORMAT} --cfa=GBRG
-./test_demosaic -i ${INPUT_FILE} -o BGGR_${WIDTH}x${HEIGHT}.rgb -w ${WIDTH} -v ${HEIGHT} --format=${FORMAT} --cfa=BGGR
+./test_demosaic -i ${INPUT_FILE} -o ${CFA}_${WIDTH}x${HEIGHT}.rgb -w ${WIDTH} -v ${HEIGHT} --format=${FORMAT} --cfa=${CFA}
